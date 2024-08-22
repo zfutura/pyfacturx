17667f929c7b3620168cdaf93ceb6f0739016c2f
Sebastian Rittau <srittau@rittau.biz>
2024-08-22 17:26:56 +0200

    Add a first test

---
+++ src/facturx/test-data/README.md
@@ -0,0 +1,6 @@
+This directory contains test XML files from the ZUGFeRD distribution
+for testing. These files are compared with the generated XML files
+during unit tests.
+
+The files are licensed indepently from the PyFactur-X project. Please see
+the individual files for their respective licenses.
---
+++ src/facturx/test_generate.py
@@ -0,0 +1,65 @@
+import xml.etree.ElementTree as ET
+from collections.abc import Callable
+from datetime import date
+from decimal import Decimal
+from pathlib import Path
+
+import pytest
+
+from facturx.generate import generate_et
+from facturx.type_codes import DocumentTypeCode
+
+from .model import MinimumInvoice, PostalAddress, TradeParty
+
+
+def minimum_rechnung() -> MinimumInvoice:
+    return MinimumInvoice(
+        invoice_number="471102",
+        type_code=DocumentTypeCode.INVOICE,
+        invoice_date=date(2020, 3, 5),
+        seller=TradeParty(
+            "Lieferant GmbH",
+            PostalAddress("DE"),
+            tax_number="201/113/40209",
+            vat_id="DE123456789",
+        ),
+        buyer=TradeParty("Kunden AG Frankreich", None),
+        currency_code="EUR",
+        tax_basis_total_amount=Decimal("198.00"),
+        tax_total_amount=Decimal("37.62"),
+        grand_total_amount=Decimal("235.62"),
+        due_payable_amount=Decimal("235.62"),
+    )
+
+
+@pytest.mark.parametrize(
+    "invoice, filename", [(minimum_rechnung, "MINIMUM_Rechnung.xml")]
+)
+def test_generate(invoice: Callable[[], MinimumInvoice], filename: str):
+    our_xml = _generate_xml(invoice())
+    their_xml = _read_xml(filename)
+    assert our_xml == their_xml
+
+
+def _generate_xml(invoice: MinimumInvoice) -> str:
+    tree = generate_et(invoice)
+    tree.attrib = dict(sorted(tree.attrib.items()))
+    ET.indent(tree)
+    return ET.tostring(tree, encoding="unicode")
+
+
+def _read_xml(filename: str) -> str:
+    path = Path(__file__).parent / "test-data" / filename
+    register_all_namespaces(path)
+    tree = ET.parse(path).getroot()
+    tree.attrib = dict(sorted(tree.attrib.items()))
+    ET.indent(tree)
+    return ET.tostring(tree, encoding="unicode")
+
+
+def register_all_namespaces(path: Path) -> None:
+    namespaces = dict(
+        [node for _, node in ET.iterparse(path, events=["start-ns"])]
+    )
+    for ns in namespaces:
+        ET.register_namespace(ns, namespaces[ns])
