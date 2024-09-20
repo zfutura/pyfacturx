63fd871d57964681c193042f72eed863e4ede5a7
Sebastian Rittau <srittau@rittau.biz>
2024-09-20 16:29:01 +0200

    Add docstring to FileRelationship

--- src/facturx/pdf_extract.py
+++ src/facturx/pdf_extract.py
@@ -16,6 +16,16 @@ XRECHNUNG_FILENAME: Final = "xrechnung.xml"
 
 
 class FileRelationship(Enum):
+    """The relationship between a PDF file and an embedded file.
+
+    * DATA: The embedded file contains data that is displayed in the PDF file.
+    * SOURCE: The embedded file is the source data for the PDF file.
+    * ALTERNATIVE: The embedded file is an alternative representation of
+      the PDF file.
+    * SUPPLEMENT: The embedded file contains information that supplements
+      the data in the PDF file.
+    """
+
     DATA = "Data"
     SOURCE = "Source"
     ALTERNATIVE = "Alternative"
