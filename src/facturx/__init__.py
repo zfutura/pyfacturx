3b77ebcdd8df8f6b497d43a28f83939fc9672709
Sebastian Rittau <srittau@rittau.biz>
2024-09-20 11:47:23 +0200

    Add {FACTURX,ZUGFERD}_VERSION constants

--- src/facturx/__init__.py
+++ src/facturx/__init__.py
@@ -1,3 +1,5 @@
+from typing import Final
+
 from .exc import *  # noqa: F403
 from .format import format_invoice_as_text as format_invoice_as_text
 from .generate import (
@@ -7,3 +9,6 @@ from .generate import (
 from .model import *  # noqa: F403
 from .money import Money as Money
 from .parse import parse_xml as parse_xml
+
+FACTURX_VERSION: Final = "1.0.07"
+ZUGFERD_VERSION: Final = "2.3"
